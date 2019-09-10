ea69fe86c9a8b06698b4a4ff5f5f1334877265f6
annahinrichsen <annaahinrichsen@gmail.com>
2019-09-10 12:47:52 +0200

    Nice exercise ala Kenneth

--- git-test.py
+++ git-test.py
@@ -1,2 +1,64 @@
-git_test
+
 print ("hello")
+
+#learn Python
+d = 2+2
+
+#%%
+
+
+
+#%%
+d = 2+2
+
+#strings
+"a string" + "another string"
+
+print("d is" + str(d))
+
+#format
+print(f"d is {d} which is a number of type int")
+
+#type
+
+#what type is d?
+type(d)
+(f"d is {d} which is a number of type {type(d)}")
+
+string = "this is a string"
+string.split(" ")
+
+a_list_of_strings = string.split(" ")
+a_list_of_strings[0]
+
+
+#make a funtion 
+
+def print_hello():
+    print("hello world")
+
+def add_hello_to_string(string):
+    return string + "hello"
+
+
+#%%
+#create a string which uses something from westworld
+#use an object 
+
+westworld = "I am a robot"
+westworld + " , Yes you are"
+westworld * 3
+
+westworld.split(" ")
+westworld.count("")
+westworld.title( )
+
+#print string
+number_string = "2 + 3 = 5"
+print(number_string)
+
+d = 2+3
+print(d)
+print("2 + 3 = " + str(d))
+
+#%%
